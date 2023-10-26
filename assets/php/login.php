e09137dbe006f1b61f4a4363e8e9dd91fae84ef6
Nayrlen Alves <nayrlenalves4@gmail.com>
2023-10-25 21:50:44 -0300

    Update login.php

--- assets/php/login.php
+++ assets/php/login.php
@@ -8,26 +8,28 @@
         $senha = $_POST['senha']; 
     }
 
-    $sql = "SELECT * FROM usuarios WHERE usuario = ? AND senha = ?";
+    $sql = "SELECT * FROM usuarios WHERE usuario = ?";
 
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ss", $usuario, $senha);
+    $stmt->bind_param("s", $usuario);
     $stmt->execute();
 
     $result = $stmt->get_result();
 
     if ($result->num_rows === 1) {
         $row = $result->fetch_assoc();
+        $hash = password_hash($senha, PASSWORD_DEFAULT);
+        $senha_bd = $row['senha'];
 
-        if (password_verify($senha, $row['senha'])) {
+        if (password_verify($hash, $senha_bd)) {
             
             $_SESSION["loggedin"] = true;
 
             header("Location: dashboard.php");
             exit;
+        }else{
+            $error = "Usuário e/ou senha incorretos";
         } 
-    }else{
-        $error = "Usuário e/ou senha incorretos";
     }
     
 ?>
